54df4670458989ca57d234b7bd343d4b4037f4c1
Marco Antônio Meira de Lima <88397083+Marcokbc@users.noreply.github.com>
2023-04-18 10:56:45 -0300

    First Migration

---
+++ Logistica/Logistica.Infraestructure/Context/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistica.Infraestructure.Context
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-IVE2OFS\\SQLEXPRESS;Initial Catalog=LogisticaDB;Integrated Security=True;TrustServerCertificate=True");
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Logistica/Logistica.Infraestructure/Migrations/20230418135036_MigracaioInicial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Logistica.Infraestructure.Migrations
+{
+    public partial class MigracaioInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Origem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Destino = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedido", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rota",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCidade = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DataRota = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IdPedido = table.Column<int>(type: "int", nullable: false),
+                    PedidoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rota", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rota_Pedido_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedido",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rota_PedidoId",
+                table: "Rota",
+                column: "PedidoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rota");
+
+            migrationBuilder.DropTable(
+                name: "Pedido");
+        }
+    }
+}
